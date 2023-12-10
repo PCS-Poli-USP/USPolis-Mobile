95e69bd251979db174abdd8b94f21a583b2139ad
JorgeHabib <jorgehabib.khouri@gmail.com>
2023-12-10 11:55:44 -0300

    Refactor MyClasses component

--- src/screens/MyClasses/index.tsx
+++ src/screens/MyClasses/index.tsx
@@ -1,49 +1,83 @@
-import FeatherIcons from "@expo/vector-icons/Feather";
-import { useNavigation } from "@react-navigation/native";
-import { DayClasses } from "./DayClasses";
-import { Layout, ClassModalDetails, Box, VStack, Typography, Pressable } from "@/components";
-import { useClasses } from "@/hooks/react-query/useClasses";
-import { useState } from "react";
-import { useSchedule } from "@/hooks/useSchedule";
-import { scheduleFactory } from "./utils";
-import { useTheme } from "@shopify/restyle";
-import { Theme } from "@/theme/theme";
-import { IClass } from "@/dtos";
-import { ClassesGroupedByWeekday } from "@/dtos/classes";
-import { logger } from "@/services/logger";
-import React from "react";
+import FeatherIcons from '@expo/vector-icons/Feather'
+import { NavigationProp, useNavigation } from '@react-navigation/native'
+import { DayClasses } from './DayClasses'
+import {
+  Layout,
+  ClassModalDetails,
+  Box,
+  VStack,
+  Typography,
+  Pressable,
+  Button,
+} from '@/components'
+import { useClasses } from '@/hooks/react-query/useClasses'
+import React, { useState } from 'react'
+import { useSchedule } from '@/hooks/useSchedule'
+import { scheduleFactory } from './utils'
+import { useTheme } from '@shopify/restyle'
+import { Theme } from '@/theme/theme'
+import { IClass } from '@/dtos'
+import { ClassesGroupedByWeekday } from '@/dtos/classes'
+import { logger } from '@/services/logger'
+import { AppRoutesType } from '@/routes/app.routes'
 
-const MemoDayClasses = React.memo(DayClasses);
+const MemoDayClasses = React.memo(DayClasses)
 
 export const MyClasses = () => {
-  const [selectedClass, setSelectedClass] = useState<IClass | undefined>();
+  const [selectedClass, setSelectedClass] = useState<IClass | undefined>()
   const [isClassModalOpen, setIsClassModalOpen] = useState(false)
 
-  const { colors } = useTheme<Theme>();
-  const { data: classes } = useClasses();
-  const { schedule } = useSchedule();
-  const navigation = useNavigation();
+  const { data: classes } = useClasses()
+  const { schedule } = useSchedule()
+  const navigation = useNavigation<NavigationProp<AppRoutesType>>()
 
-  const classesGroupedByWeekday = scheduleFactory(schedule, classes);
+  const classesGroupedByWeekday = scheduleFactory(schedule, classes)
 
   const onOpenModal = (classId: string, className: string) => {
     const sclass = classes?.find((c) => c.id === classId)
 
-    logger.logEvent('Aula Visualizada', { class: className, screen: 'Minhas Aulas' })
+    logger.logEvent('Aula Visualizada', {
+      class: className,
+      screen: 'Minhas Aulas',
+    })
 
-    setSelectedClass(sclass);
-    setIsClassModalOpen(true);
-  };
+    setSelectedClass(sclass)
+    setIsClassModalOpen(true)
+  }
 
   return (
-    <Box flex={1} backgroundColor="graySeven" paddingHorizontal="s" paddingTop={'m'} paddingBottom={'s'}>
+    <Box
+      flex={1}
+      backgroundColor="graySeven"
+      paddingHorizontal="s"
+      paddingTop={'m'}
+      paddingBottom={'s'}
+    >
       <Layout>
         <ClassModalDetails
           sclass={selectedClass}
           isOpen={isClassModalOpen}
           onClose={() => setIsClassModalOpen(false)}
         />
-        <VStack height={"100%"}>
+        <VStack height={'100%'}>
+          {!classesGroupedByWeekday.length && (
+            <>
+              <Typography
+                color="grayThree"
+                fontSize={14}
+                textAlign="center"
+                marginTop={'m'}
+                marginBottom="m"
+              >
+                Nenhuma aula adicionada
+              </Typography>
+              <Button
+                title={'Adicionar disciplina'}
+                variant="outlined"
+                onPress={() => navigation.navigate('Home')}
+              />
+            </>
+          )}
           {classesGroupedByWeekday.map((group: ClassesGroupedByWeekday) => (
             <MemoDayClasses
               onOpenModal={onOpenModal}
@@ -52,12 +86,7 @@ export const MyClasses = () => {
             />
           ))}
         </VStack>
-        {!classesGroupedByWeekday.length && (
-          <Typography color="grayThree" fontSize={14} textAlign="center" marginTop={'m'}>
-            Nenhuma aula adicionada
-          </Typography>
-        )}
       </Layout>
     </Box>
-  );
-};
+  )
+}
