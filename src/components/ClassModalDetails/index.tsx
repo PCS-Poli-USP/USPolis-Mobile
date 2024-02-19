388aab2ba701b33ea3af3295a9b3ce2f64aaf672
FastShowDev <zlfastshow@hotmail.com>
2024-02-19 20:10:41 -0300

    Change professors to a vector

--- src/components/ClassModalDetails/index.tsx
+++ src/components/ClassModalDetails/index.tsx
@@ -1,22 +1,22 @@
-import { useSchedule } from '@/hooks/useSchedule'
-import { AppRoutesType } from '@/routes/app.routes'
-import FeatherIcons from '@expo/vector-icons/Feather'
-import { NavigationProp, useNavigation } from '@react-navigation/native'
-import { differenceInDays, format, parseISO } from 'date-fns'
-import { Pressable } from 'react-native'
-import { Button } from '../Button'
-import { sortEventsByScheduleTime } from './utils'
-import { Building, IClass } from '../../dtos/classes'
-import { Theme } from '@/theme/theme'
-import { useTheme } from '@shopify/restyle'
-import { Box, HStack, Typography, VStack } from '../ui'
-import { logger } from '@/services/logger'
-import { Modal } from '../Modal'
+import { useSchedule } from "@/hooks/useSchedule";
+import { AppRoutesType } from "@/routes/app.routes";
+import FeatherIcons from "@expo/vector-icons/Feather";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { differenceInDays, format, parseISO } from "date-fns";
+import { Pressable } from "react-native";
+import { Button } from "../Button";
+import { sortEventsByScheduleTime } from "./utils";
+import { Building, IClass } from "../../dtos/classes";
+import { Theme } from "@/theme/theme";
+import { useTheme } from "@shopify/restyle";
+import { Box, HStack, Typography, VStack } from "../ui";
+import { logger } from "@/services/logger";
+import { Modal } from "../Modal";
 
 interface ClassModalDetailsProps {
-  sclass?: IClass | null
-  isOpen: boolean
-  onClose: () => void
+  sclass?: IClass | null;
+  isOpen: boolean;
+  onClose: () => void;
 }
 
 export const ClassModalDetails = ({
@@ -24,40 +24,40 @@ export const ClassModalDetails = ({
   isOpen,
   onClose,
 }: ClassModalDetailsProps) => {
-  const navigation = useNavigation<NavigationProp<AppRoutesType>>()
-  const { colors } = useTheme<Theme>()
-  const { schedule, toggleClassOnSchedule } = useSchedule()
+  const navigation = useNavigation<NavigationProp<AppRoutesType>>();
+  const { colors } = useTheme<Theme>();
+  const { schedule, toggleClassOnSchedule } = useSchedule();
 
   const handleToggleClassOnSchedule = (classId: string, className: string) => {
     if (schedule.includes(classId)) {
-      logger.logEvent('Aula Removida no Cronograma', { class: className })
+      logger.logEvent("Aula Removida no Cronograma", { class: className });
     } else {
-      logger.logEvent('Aula Adicionada do Cronograma', { class: className })
+      logger.logEvent("Aula Adicionada do Cronograma", { class: className });
     }
 
-    toggleClassOnSchedule(classId)
-    logger.setUserProperty('classes_on_schedule', schedule.length.toString())
-  }
+    toggleClassOnSchedule(classId);
+    logger.setUserProperty("classes_on_schedule", schedule.length.toString());
+  };
 
-  if (!sclass) return <></>
+  if (!sclass) return <></>;
 
   const progressValue =
     differenceInDays(new Date(), parseISO(sclass.end_period)) > 0
       ? (differenceInDays(new Date(), parseISO(sclass.end_period)) /
           differenceInDays(
             parseISO(sclass.start_period),
-            parseISO(sclass.end_period),
+            parseISO(sclass.end_period)
           )) *
         100
-      : 0
+      : 0;
 
   const navigateToMap = (building: Building, floor: number) => {
-    navigation.navigate('Maps', {
+    navigation.navigate("Maps", {
       building,
       floor,
-    })
-    onClose()
-  }
+    });
+    onClose();
+  };
 
   return (
     <Box flex={1}>
@@ -74,50 +74,53 @@ export const ClassModalDetails = ({
         // style={{ margin: 0 }}
       >
         <Box
-          width={'100%'}
+          width={"100%"}
           borderTopLeftRadius={8}
           borderTopRightRadius={8}
           backgroundColor="graySeven"
           position="absolute"
           bottom={0}
-          paddingHorizontal={'s'}
+          paddingHorizontal={"s"}
           paddingVertical="l"
         >
           <Box backgroundColor="graySeven" borderBottomColor="transparent">
-            <VStack alignItems="center" marginBottom={'m'}>
+            <VStack alignItems="center" marginBottom={"m"}>
               <Typography
-                variant={'heading'}
+                variant={"heading"}
                 color="white"
                 fontSize={20}
-                marginBottom={'s'}
+                marginBottom={"s"}
               >
                 {sclass.subject_code}
               </Typography>
               <Typography
-                variant={'heading'}
+                variant={"heading"}
                 color="white"
                 fontSize={18}
-                marginBottom={'s'}
+                marginBottom={"s"}
               >
                 {sclass.subject_name}
               </Typography>
-              <Typography color="grayTwo" fontSize={14}>
+              <Typography color="grayTwo" fontSize={14} marginBottom={"s"}>
                 Turma {sclass.class_code.slice(-2)}
+              </Typography>
+              <Typography color="grayTwo" fontSize={14}>
+                {sclass.professors.join(", ")}
               </Typography>
             </VStack>
           </Box>
           <Box
             backgroundColor="graySeven"
-            paddingHorizontal={'m'}
-            paddingBottom={'s'}
+            paddingHorizontal={"m"}
+            paddingBottom={"s"}
           >
             <VStack backgroundColor="graySeven" marginBottom="m">
               {sclass.schedule
                 .sort(sortEventsByScheduleTime)
                 .map((event, index) => (
-                  <Box key={`${event.id}-${index}`} marginBottom={'s'}>
-                    <Typography variant={'heading'} color="white" mb={'xs'}>
-                      {event.week_day}, das {event.start_time} às{' '}
+                  <Box key={`${event.id}-${index}`} marginBottom={"s"}>
+                    <Typography variant={"heading"} color="white" mb={"xs"}>
+                      {event.week_day}, das {event.start_time} às{" "}
                       {event.end_time}
                     </Typography>
                     <Pressable
@@ -129,9 +132,9 @@ export const ClassModalDetails = ({
                         alignItems="center"
                         backgroundColor="grayFive"
                         borderRadius={8}
-                        padding={'s'}
+                        padding={"s"}
                       >
-                        <VStack gap={'xs'}>
+                        <VStack gap={"xs"}>
                           <Typography color="white">
                             Prédio: {event.building}
                           </Typography>
@@ -139,8 +142,8 @@ export const ClassModalDetails = ({
                             Sala: {event.classroom}
                           </Typography>
                         </VStack>
-                        <HStack alignItems={'center'}>
-                          <Typography color={'primary'} marginRight="s">
+                        <HStack alignItems={"center"}>
+                          <Typography color={"primary"} marginRight="s">
                             Ver no mapa
                           </Typography>
                           <FeatherIcons
@@ -164,31 +167,31 @@ export const ClassModalDetails = ({
                   <Box>
                     <Typography color="grayTwo">Início</Typography>
                     <Typography color="white">
-                      {format(parseISO(sclass.start_period), 'dd/MM/yyyy')}
+                      {format(parseISO(sclass.start_period), "dd/MM/yyyy")}
                     </Typography>
                   </Box>
                   <Box>
                     <Typography color="grayTwo">Fim</Typography>
                     <Typography color="white">
-                      {format(parseISO(sclass.end_period), 'dd/MM/yyyy')}
+                      {format(parseISO(sclass.end_period), "dd/MM/yyyy")}
                     </Typography>
                   </Box>
                 </Box>
                 {/* <Progress
                   value={progressValue}
-                  size="md"
+                  size='md'
                   mt={1}
                   _filledTrack={{
-                    bg: "green.700",
+                    bg: 'green.700',
                   }}
                 /> */}
               </Box>
               <Button
-                variant={'outlined'}
+                variant={"outlined"}
                 title={
                   schedule.includes(sclass.id)
-                    ? 'Remover de minhas disciplinas'
-                    : 'Adicionar em minhas disciplinas'
+                    ? "Remover de minhas disciplinas"
+                    : "Adicionar em minhas disciplinas"
                 }
                 onPress={() =>
                   handleToggleClassOnSchedule(sclass.id, sclass.subject_name)
@@ -199,5 +202,5 @@ export const ClassModalDetails = ({
         </Box>
       </Modal>
     </Box>
-  )
-}
+  );
+};
--- src/hooks/react-query/useEvents.ts
+++ src/hooks/react-query/useEvents.ts
@@ -1,18 +1,25 @@
-import { IEvent } from '@/dtos/events'
-import api from '@/services/api'
-import { useQuery } from 'react-query'
+import { IEvent } from "@/dtos/events";
+import api from "@/services/api";
+import { useQuery } from "react-query";
 
 export const useEvents = () => {
   const query = useQuery(
-    ['events'],
+    ["events"],
     async () => {
-      const response = await api.get<IEvent[]>('/institutional-events')
-      return response.data
+      const response = await api
+        .get<IEvent[]>("/institutional-events")
+        .then((response) => {
+          return response.data;
+        })
+        .catch((error) => {
+          console.log("Erro: ", error);
+          return [];
+        });
     },
     {
       keepPreviousData: true,
-    },
-  )
+    }
+  );
 
-  return query
-}
+  return query;
+};
