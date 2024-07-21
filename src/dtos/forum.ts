53a852fe64b90f031ed485bbb02aec0ff7b0d2c9
VitorMakiyama <jv.makiyama@usp.br>
2024-07-21 20:05:12 -0300

    Linked the forum to MobileUser in the backend
    A post is now associated with a user

--- src/dtos/forum.ts
+++ src/dtos/forum.ts
@@ -1,7 +1,14 @@
-export type PostDTO = {
-    id: string;
-    author: string;
+export type PostRequest = {
+    user_id: number;
     content: string;
-    created_at: string;
-    event_id: string;
+    class_id: number;
 };
+
+export type PostResponse = {
+    id: string;
+    user_id: number;
+    user_name: string;
+    content: string;
+    class_id: number;
+    created_at: string;
+}
